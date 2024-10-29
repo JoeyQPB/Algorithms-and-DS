c2ff5c2b8f1dad5ce7a7ccb3bc698ddb8e72f406
JoeyQPB <joey_qpb@hotmail.com>
2024-10-29 14:14:13 -0300

    binary search

--- src/main/java/joey/com/algorithms/BinarySearch.java
+++ src/main/java/joey/com/algorithms/BinarySearch.java
@@ -36,6 +36,11 @@ public class BinarySearch<E extends Comparable<E>> {
         public void setFind(Boolean find) {
             this.find = find;
         }
+
+        @Override
+        public String toString() {
+            return "VALUE: " + value + " - TRIES: " + tries + " - FIND: " + find;
+        }
     }
 
     public Response<E> search(E[] arr, E el) {
@@ -53,6 +58,7 @@ public class BinarySearch<E extends Comparable<E>> {
             if (cpt == 0) {
                 resp.setFind(true);
                 resp.setValue(pointer);
+                break;
             } else if (cpt < 0) {
                 start = mid + 1;
             } else {
@@ -63,4 +69,46 @@ public class BinarySearch<E extends Comparable<E>> {
         return resp;
     }
 
+    public static Response<Integer> searchRec(Integer[] arr, int el, int start, int end, Response<Integer> resp) {
+        if (end >= start) {
+            resp.addTry();
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == el) {
+                resp.setValue(arr[mid]);
+                resp.setFind(true);
+            } else if (arr[mid] > el) {
+               searchRec(arr, el, start, mid - 1, resp);
+            } else {
+                searchRec(arr, el, mid + 1, end, resp);
+            }
+        }
+        return resp;
+    }
 }
+
+
+//Integer[] arr = new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//BinarySearch<Integer> binarySearch = new BinarySearch<>();
+//
+//        System.out.println("BINARY SEARCH");
+//        System.out.println("TRIES: 3    - " + binarySearch.search(arr, 1));
+//        System.out.println("TRIES: 1    - " + binarySearch.search(arr, 5));
+//        System.out.println("TRIES: 3    - " + binarySearch.search(arr, 3));
+//        System.out.println("TRIES: 2    - " + binarySearch.search(arr, 2));
+//        System.out.println("TRIES: 2    - " + binarySearch.search(arr, 8));
+//        System.out.println("TRIES: 4    - " + binarySearch.search(arr, 7));
+//        System.out.println("TRIES: 4    - " + binarySearch.search(arr, 10));
+//        System.out.println("TRIES: 4    - " + binarySearch.search(arr, 11));
+//
+//        System.out.println();
+//        System.out.println("BINARY SEARCH REC");
+//
+//        System.out.println("TRIES: 3    - " + BinarySearch.searchRec(arr, 1, 0, arr.length -1, new BinarySearch.Response<>(null, 0, false)));
+//        System.out.println("TRIES: 1    - " + BinarySearch.searchRec(arr, 5, 0, arr.length -1, new BinarySearch.Response<>(null, 0, false)));
+//        System.out.println("TRIES: 3    - " + BinarySearch.searchRec(arr, 3, 0, arr.length -1, new BinarySearch.Response<>(null, 0, false)));
+//        System.out.println("TRIES: 2    - " + BinarySearch.searchRec(arr, 2, 0, arr.length -1, new BinarySearch.Response<>(null, 0, false)));
+//        System.out.println("TRIES: 2    - " + BinarySearch.searchRec(arr, 8, 0, arr.length -1, new BinarySearch.Response<>(null, 0, false)));
+//        System.out.println("TRIES: 4    - " + BinarySearch.searchRec(arr, 7, 0, arr.length -1, new BinarySearch.Response<>(null, 0, false)));
+//        System.out.println("TRIES: 4    - " + BinarySearch.searchRec(arr, 10, 0, arr.length -1, new BinarySearch.Response<>(null, 0, false)));
+//        System.out.println("TRIES: 4    - " + BinarySearch.searchRec(arr, 11, 0, arr.length -1, new BinarySearch.Response<>(null, 0, false)));
